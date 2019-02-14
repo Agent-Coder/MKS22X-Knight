f9f6a835c8b63e71a813da96e58c117880575fac
Amanda Zheng <azheng00@stuy.edu>
2019-02-14 10:10:04 -0500

    started doing the solve method

--- KnightBoard.java
+++ KnightBoard.java
@@ -14,7 +14,7 @@ public class KnightBoard{
   public String toString(){
     String s="";
     for (int i=0;i<sequence.length;i++){
-      s+="\n"
+      s+="\n";
       for (int j=0;j<sequence[0].length;j++){
         if (sequence[i][j]>9){
           s+="_"+sequence[i][j]+" ";
@@ -31,26 +31,84 @@ public class KnightBoard{
   //@throws IllegalStateException when the board contains non-zero values.
   //@throws IllegalArgumentException when either parameter is negative or out of bounds.
   public boolean solve(int startingRow, int startingCol){
-    return solveKnight(startingRow,startingCol);
+    return solveH(startingRow,startingCol,0);
   }
-  public boolean SolveKnight(int r,int c,){
 
-  }
-  public boolean addKnight(int r,int c,int amount){
-    boolean place=false;
-    if(amount==0){
-    if (r+2<board.length){
-
-
-      }
+  public boolean addKnight(int r,int c,int num){
+    if (r>=0&&r<sequence.length&&c>=0&&c<sequence.length&&sequence[r][c]==0){
+      sequence[r][c]=num;
+      return true;
     }
+    return false;
+  }
+  public boolean removeKnight(int r,int c){
+    if (r>=0&&r<sequence.length&&c>=0&&c<sequence.length){
+      sequence[r][c]=0;
+      return true;
+    }
+    return false;
   }
 
-  //@throws IllegalStateException when the board contains non-zero values.
+  //@throws IllegalStateException when the sequence contains non-zero values.
   //@throws IllegalArgumentException when either parameter is negative or out of bounds.
-  public int countSolutions(int startingRow, int startingCol)
+  //public int countSolutions(int startingRow, int startingCol)
 
   //Suggestion:
-  private boolean solveH(int row ,int col, int level)
+  private boolean solveH(int row ,int col, int level){
+    if(level==sequence.length*sequence[0].length+1){
+      return true;
+    }
+    if(addKnight(row,col,level)){
+      System.out.println(this.toString());
+      if (solveH(row+2 ,col+1,level+1)){
+        System.out.println("1"+this.toString());
+        return true;
+      }
+      if(solveH(row+2 ,col-1,level+1)){
+        System.out.println("2"+this.toString());
+        return true;
+      }
+      if(solveH(row-2 ,col+1,level+1)){
+        System.out.println("3"+this.toString());
+        return true;
+      }
+      if(solveH(row-2 ,col-1,level+1)){
+        System.out.println("4"+this.toString());
+        return true;
+      }
+      if(solveH(row+1 ,col+2,level+1)){
+        System.out.println("5"+this.toString());
+        return true;
+      }
+      if(solveH(row+1 ,col-2,level+1)){
+        System.out.println("6"+this.toString());
+        return true;
+      }
+      if(solveH(row-1 ,col+2,level+1)){
+        System.out.println("7"+this.toString());
+        return true;
+      }
+      if(solveH(row-1 ,col-2,level+1)){
+        System.out.println("8"+this.toString());
+        return true;
+      }
+      return false;
+
+
+            //solveH(row+2 ,col-1,level+1)||
+            //solveH(row-2 ,col+1,level+1)||
+            //solveH(row-2 ,col-1,level+1)||
+            //solveH(row+1 ,col+2,level+1)||
+            //solveH(row+1 ,col-2,level+1)||
+            //solveH(row-1 ,col+2,level+1)||
+            //solveH(row-1 ,col-2,level+1);
+    }
+    removeKnight(row,col);
+    return false;
+  }
   //level is the # of the knight
+  public static void main(String[] args) {
+    KnightBoard k=new KnightBoard(3,3);
+    System.out.println(k.solve(0,0));
+  }
 }
