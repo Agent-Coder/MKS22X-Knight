e23497e9c1b7ad2f5a91512b5756cd0c0510145a
Amanda Zheng <azheng00@stuy.edu>
2019-02-23 13:00:48 -0500

    constructor for the board of chances works

--- Optimize.java
+++ Optimize.java
@@ -3,36 +3,42 @@ public class Optimize{
   public Optimize(int r, int c){
     board=new int[r][c];
     for(int i=0;i<board.length;i++){
-      for(int j=0;i<board[0].length;i++){
-        board[i][j]=0;
+      for(int j=0;j<board[0].length;j++){
+        this.construct(i,j);
       }
     }
-    board[0][0]=1;
-    board[r-1][0]=1;
-    board[0][c-1]=1;
-    board[r-1][c-1]=1;
-    if (r>3&&c>3){
-      board[0][0]=2;
-      board[r-1][0]=2;
-      board[0][c-1]=2;
-      board[r-1][c-1]=2;
-      for(int a=1;a<board[0].length-1;a++){
-        if(a==1||a==board[0].length-2){
-          board[0][a]=3;
-          board[a][0]=3;
-          board[0][board[0].length-2]=3;
-          board[a][board[1].length-1]=3;
-          board[board.length-2][board[0].length-1]=3;
-          board[board.length-1][board[0].length-2]=3;
-          board[board.length-2][0]=3;
-          board[board.length-1][1]=3;
-        }
-        else{
-          board[0][a]=4;
-          board[board.length-1][a]=4;
-          board[a][0]=4;
-          board[a][board[0].length-1]=4;
-        }
+  }
+  public void construct(int r,int c){
+    if(r+2<board.length){
+      if(c+1<board[0].length){
+        board[r][c]=board[r][c]+1;
+      }
+      if(c-1>=0){
+        board[r][c]=board[r][c]+1;
+      }
+    }
+    if(r-2>=0){
+      if(c+1<board[0].length){
+        board[r][c]=board[r][c]+1;
+      }
+      if(c-1>=0){
+        board[r][c]=board[r][c]+1;
+      }
+    }
+    if(c+2<board[0].length){
+      if(r+1<board.length){
+        board[r][c]=board[r][c]+1;
+      }
+      if(r-1>=0){
+        board[r][c]=board[r][c]+1;
+      }
+    }
+    if(c-2>=0){
+      if(r+1<board.length){
+        board[r][c]=board[r][c]+1;
+      }
+      if(r-1>=0){
+        board[r][c]=board[r][c]+1;
       }
     }
   }
