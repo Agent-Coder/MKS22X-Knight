a803cbfdd0bcf4f20cf2e891d3d65d2de8e929ac
Amanda Zheng <azheng00@stuy.edu>
2019-02-25 23:13:01 -0500

    made the function that orders moves based on possibility

--- Optimize.java
+++ Optimize.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Optimize{
   private int[][] board;
   private int[][] game;
@@ -122,21 +123,33 @@ public class Optimize{
       return false;
     }
   }
-  public int findLeast(int r,int c){
-    int least=9;
-    int movenum;
-    int multiple=0
-    for(int i=0;i<8;i++){
-      if(game[r+moves[2*i]][c+moves[2*i+1]]==0&&board[r+moves[2*i]][c+moves[2*i+1]]==least){
-        movenum=i;
-        multiple=-1;
+  public ArrayList<Integer> findLeast(int r,int c){
+    ArrayList<Integer> movenums=new ArrayList<Integer>();
+    movenums.add(board[r+moves[0]][c+moves[1]]);
+    ArrayList<Integer> ordermoves=new ArrayList<Integer>();
+    ordermoves.add(0);
+    for(int i=1;i<8;i++){
+      for (int k=0;k<movenums.size();k++) {
+        if(r+moves[2*i]>=0&&c+moves[2*i+1]>=0&&r+moves[2*i]<board.length&&c+moves[2*i+1]<board[0].length){
+          if(board[r+moves[2*i]][c+moves[2*i+1]]<=movenums.get(k)){
+            movenums.add(k,board[r+moves[2*i]][c+moves[2*i+1]]);
+            ordermoves.add(k,i);
+            k=10;
+          }
+          else{
+            if(i==7){
+              movenums.add(board[r+moves[2*i]][c+moves[2*i+1]]);
+              ordermoves.add(i);
+            }
+          }
+        }
+        else{
+          k=10;
+        }
+      }
     }
-    if(game[r+moves[2*i]][c+moves[2*i+1]]==0&&board[r+moves[2*i]][c+moves[2*i+1]]<least){
-      movenum=i;
-      multiple=0;
+    return ordermoves;
   }
-  }
-}
   public String ChanceString(){
     String s="";
     for (int i=0;i<board.length;i++){
